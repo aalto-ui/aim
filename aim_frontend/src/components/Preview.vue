025e91b1c36d9c6078a345459fd3754285d320c7
mplaine <markku.laine@aalto.fi>
2018-10-18 00:45:27 +0300

    Changed screenshot size and hid scrollbars

--- aim_frontend/src/components/Preview.vue
+++ aim_frontend/src/components/Preview.vue
@@ -1,7 +1,7 @@
 <template lang="html">
   <div>
     <h2>Preview</h2>
-    <p>1280 x 720 pixels at 72 dpi (URL) or original image resized to 1280 pixels width at 72 dpi (screenshot)</p>
+    <p>1280 x 800 pixels at 72 dpi (URL) or original image resized to 1280 pixels width at 72 dpi (screenshot)</p>
     <img class="preview-img" :src="'data:image/png;base64, ' + b64" />
   </div>
 </template>
