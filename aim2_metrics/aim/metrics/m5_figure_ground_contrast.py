025917f79b9c9b99bb6b44854c7e81037449fb06
mplaine <markku.laine@aalto.fi>
2020-10-27 12:25:11 +0200

    Added the figure-ground contrast metric (m5)

--- aim2_metrics/aim/metrics/m5_figure_ground_contrast.py
+++ aim2_metrics/aim/metrics/m5_figure_ground_contrast.py
@@ -3,14 +3,14 @@
 
 """
 Metric:
-    Contour density
+    Figure-ground contrast
 
 
 Description:
-    The ratio of contour pixels to all pixels.
+    The difference in color or luminance between two adjacent areas.
 
-    Category: Visual complexity > Information amount > Visual clutter.
-    For details, see CL1 [1], A9 [2], and CL1 [3].
+    Category: Visual complexity > Information discriminability.
+    For details, see 'Figure-ground contrast' [1, 2, 3].
 
 
 Funding information and contact:
@@ -35,13 +35,24 @@ References:
         Conference on Advanced Visual Interfaces (AVI '14), pp. 153-160. ACM.
         doi: https://doi.org/10.1145/2598153.2598173
 
-    4.  Rosenholtz, R., Li, Y., and Nakano, L. (2007). Measuring Visual
-        Clutter. Journal of Vision, 7(2), 1-22.
-        doi: https://doi.org/10.1167/7.2.17
+    4.  Reber, R., Wurtz, P., and Zimmermann, T.D. (2004). Exploring "fringe"
+        Consciousness: The Subjective Experience of Perceptual Fluency and
+        its Objective Bases. Consciousness and Cognition, 13(1), 47-60.
+        doi: https://doi.org/10.1016/S1053-8100(03)00049-7
+
+    5.  Hall, R.H. and Hanna, P. (2004). The Impact of Web Page
+        Text-Background Colour Combinations on Readability, Retention,
+        Aesthetics and Behavioural Intention. Behaviour & Information
+        Technology, 23(3), 183-195.
+        doi: https://doi.org/10.1080/01449290410001669932
+
+    6.  Reber, R., Winkielman, P., and Schwarz, N. (1998). Effects of
+        Perceptual Fluency on Affective Judgments. Psychological Science,
+        9(1), 45-48. doi: https://doi.org/10.1111/1467-9280.00008
 
 
 Change log:
-    v2.0 (2020-08-25)
+    v2.0 (2020-10-24)
       * Revised implementation
 
     v1.0 (2017-05-29)
@@ -78,7 +89,7 @@ from aim.metrics.interfaces import AIMMetricInterface
 # ----------------------------------------------------------------------------
 
 __author__ = "Markku Laine, Thomas Langerak, Yuxi Zhu"
-__date__ = "2020-08-25"
+__date__ = "2020-10-24"
 __email__ = "markku.laine@aalto.fi"
 __version__ = "2.0"
 
@@ -88,23 +99,15 @@ __version__ = "2.0"
 # ----------------------------------------------------------------------------
 
 
-class Metric4(AIMMetricInterface):
+class Metric5(AIMMetricInterface):
     """
-    Metric 4: Contour density.
+    Metric 5: Figure-ground contrast.
     """
-
-    # Private constants
-    _CANNY_EDGE_DETECTION_MATLAB_LOW_THRESHOLD_DESKTOP: float = 0.11
-    _CANNY_EDGE_DETECTION_MATLAB_HIGH_THRESHOLD_DESKTOP: float = 0.27
-    _CANNY_EDGE_DETECTION_PYTHON_LOW_THRESHOLD_MOBILE: int = 50
-    _CANNY_EDGE_DETECTION_PYTHON_HIGH_THRESHOLD_MOBILE: int = 50
-    _CANNY_EDGE_DETECTION_PYTHON_MIN_THRESHOLD: int = 0
-    _CANNY_EDGE_DETECTION_PYTHON_MAX_THRESHOLD: int = 255
 
     # Public methods
-    @classmethod
+    @staticmethod
     def execute_metric(
-        cls, gui_image: str, gui_type: int = GUI_TYPE_DESKTOP
+        gui_image: str, gui_type: int = GUI_TYPE_DESKTOP
     ) -> Optional[List[Any]]:
         """
         Execute the metric.
@@ -117,7 +120,7 @@ class Metric4(AIMMetricInterface):
 
         Returns:
             Results (list of measures)
-            - Contour density (float, [0, 1])
+            - Figure-ground contrast (float, [0, 1])
         """
         # Create PIL image
         img: Image.Image = Image.open(BytesIO(base64.b64decode(gui_image)))
@@ -146,52 +149,79 @@ class Metric4(AIMMetricInterface):
             src=img_l_nparray, ksize=ksize, sigmaX=sigma, sigmaY=sigma
         )
 
+        # Generate levels (with a step of 1) and luminance levels; from 0.1
+        # to 0.7 for desktop GUIs and from 0.05 to 0.65 for mobile GUIs with
+        # a step of 0.1
+        levels: List[int] = [level for level in range(1, 8, 1)]
+        luminance_levels: List[float] = (
+            [round(0.1 * (level - 0.5), 2) for level in levels]
+            if gui_type == GUI_TYPE_MOBILE
+            else [round(0.1 * level, 1) for level in levels]
+        )
+
         # Canny edge detection parameters
-        low_threshold: float = (
-            cls._CANNY_EDGE_DETECTION_PYTHON_LOW_THRESHOLD_MOBILE
-            if gui_type == GUI_TYPE_MOBILE
-            else round(
-                CANNY_EDGE_DETECTION_PYTHON_MAX_THRESHOLD
-                * cls._CANNY_EDGE_DETECTION_MATLAB_LOW_THRESHOLD_DESKTOP,
-                2,
+        high_thresholds: List[float] = [
+            round(
+                CANNY_EDGE_DETECTION_PYTHON_MAX_THRESHOLD * luminance_level, 2
             )
-        )  # 50 or 28.05 [0, 255] for mobile and desktop GUIs, respectively
-        high_threshold: float = (
-            cls._CANNY_EDGE_DETECTION_PYTHON_HIGH_THRESHOLD_MOBILE
-            if gui_type == GUI_TYPE_MOBILE
-            else round(
-                CANNY_EDGE_DETECTION_PYTHON_MAX_THRESHOLD
-                * cls._CANNY_EDGE_DETECTION_MATLAB_HIGH_THRESHOLD_DESKTOP,
-                2,
-            )
-        )  # 50 or 68.85 [0, 255] for mobile and desktop GUIs, respectively
-        # Note 1: According to [3], low and high thresholds for desktop GUIs
-        # were set to 0.11 and 0.27 (MATLAB), respectively. However, MATLAB's
-        # edge() function (https://www.mathworks.com/help/images/ref/edge.html)
-        # with the 'Canny' method expects thresholds to be in the range of
-        # [0, 1], whereas OpenCV's Canny() function
+            for luminance_level in luminance_levels
+        ]
+        low_thresholds: List[float] = [
+            round(0.4 * high_threshold, 2)
+            for high_threshold in high_thresholds
+        ]  # 40% of high thresholds
+        # Note 1: According to [2, 3], high thresholds varied from 0.1 to 0.7
+        # and from 0.05 to 0.65 (MATLAB) with a step of 0.1 for desktop GUIs
+        # and mobile GUIs, respectively. However, MATLAB's edge() function
+        # (https://www.mathworks.com/help/images/ref/edge.html) with the
+        # 'Canny' method expects thresholds to be in the range of [0, 1],
+        # whereas OpenCV's Canny() function
         # (https://docs.opencv.org/4.4.0/dd/d1a/group__imgproc__feature.html#ga04723e007ed888ddf11d9ba04e2232de)
         # does not specify the scale. According to the following link
         # (https://www.pyimagesearch.com/2015/04/06/zero-parameter-automatic-canny-edge-detection-with-python-and-opencv/),
         # OpenCV's Canny() function expects thresholds to be in the range of
         # [0, 255], though.
-        # Note 2: According to a personal discussion with Miniukovich, both
-        # low and high thresholds for mobile GUIs were set to 50 on the 0-255
-        # scale. This information is not present in [2].
 
-        # Detect edges
-        img_contours_nparray: np.ndarray = cv2.Canny(
-            image=img_blurred_nparray,
-            threshold1=low_threshold,
-            threshold2=high_threshold,
-        )
+        # Count edge pixels per level
+        edge_pixels_per_level: List[int] = []  # Edge strengths
+        for index in range(len(luminance_levels)):
+            # Detect edges
+            img_contours_nparray: np.ndarray = cv2.Canny(
+                image=img_blurred_nparray,
+                threshold1=low_thresholds[index],
+                threshold2=high_thresholds[index],
+            )
+            n_edge_pixels: int = np.count_nonzero(img_contours_nparray)
+            edge_pixels_per_level.append(n_edge_pixels)
 
-        # Calculate contour density
-        img_shape: Tuple[int, int] = img_contours_nparray.shape
-        n_all_pixels: int = img_shape[0] * img_shape[1]  # height * width
-        n_contour_pixels: int = np.count_nonzero(img_contours_nparray)
-        contour_density: float = n_contour_pixels / n_all_pixels
+        # Compute the sum of weighted difference in pixels between each
+        # successive level
+        sum_of_weighted_differences: float = 0.0
+        for index in range(len(edge_pixels_per_level) - 1):
+            level: int = levels[index]
+            # The difference in pixels between each successive level
+            difference: int = (
+                edge_pixels_per_level[index] - edge_pixels_per_level[index + 1]
+            )
+            weight: float = 1 - (
+                (level - 1) / (len(edge_pixels_per_level) - 1)
+            )
+            # The weighted difference in pixels between each successive level
+            weighted_difference: float = difference * weight
+            sum_of_weighted_differences += weighted_difference
+
+        # Compute figure-ground contrast
+        # (normalized sum of weighted differences)
+        try:
+            figure_ground_contrast: float = sum_of_weighted_differences / (
+                edge_pixels_per_level[0] - edge_pixels_per_level[-1]
+            )
+        except ZeroDivisionError:
+            figure_ground_contrast = 0.0
+        # Note 1: According to the equation in [3], the denominator is
+        # supposed to be E1 - E6, but the text description says 0.1 - 0.7
+        # (i.e., E1 - E7). The latter should be correct.
 
         return [
-            contour_density,
+            figure_ground_contrast,
         ]
---
+++ aim2_metrics/tests/metrics/test_m5.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Tests for the 'Figure-ground contrast' metric (m5).
+"""
+
+
+# ----------------------------------------------------------------------------
+# Imports
+# ----------------------------------------------------------------------------
+
+# Standard library modules
+import pathlib
+from typing import Any, List, Optional
+
+# Third-party modules
+import pytest
+
+# First-party modules
+import aim.core.utils as aim_utils
+from aim.metrics.m5_figure_ground_contrast import Metric5
+from tests.core.constants import DATA_TESTS_DIR
+
+# ----------------------------------------------------------------------------
+# Metadata
+# ----------------------------------------------------------------------------
+
+__author__ = "Markku Laine"
+__date__ = "2020-10-27"
+__email__ = "markku.laine@aalto.fi"
+__version__ = "1.0"
+
+
+# ----------------------------------------------------------------------------
+# Tests
+# ----------------------------------------------------------------------------
+
+
+@pytest.mark.parametrize(
+    ["input_value", "expected_result"],
+    [
+        ("aalto.fi_website.png", [0.7360909596941244]),
+        ("transparent.png", [0.0]),  # transparent -> white pixels
+        ("white.png", [0.0]),
+        ("black.png", [0.0]),
+        ("gray.png", [0.0]),
+        ("red.png", [0.0]),
+        ("green.png", [0.0]),
+        ("blue.png", [0.0]),
+        (
+            "white_50_transparent_50.png",
+            [0.0],
+        ),  # transparent -> white pixels
+        (
+            "black_50_transparent_50.png",
+            [0.0],
+        ),  # transparent -> white pixels
+        ("white_50_black_50.png", [0.0]),
+        ("red_50_green_50.png", [0.5]),
+        ("green_50_blue_50.png", [0.0]),
+        ("blue_50_red_50.png", [0.8333333333333335]),
+        ("4_high-contrast_shades_of_gray.png", [0.6666666666666667]),
+        ("4_low-contrast_shades_of_gray.png", [0.0]),
+    ],
+)
+def test_figure_ground_contrast_desktop(
+    input_value: str, expected_result: List[Any]
+) -> None:
+    """
+    Test figure-ground contrast (desktop GUIs).
+
+    Args:
+        input_value: GUI image file name
+        expected_result: Expected result (list of measures)
+    """
+    # Build GUI image file path
+    gui_image_filepath: pathlib.Path = (
+        pathlib.Path(DATA_TESTS_DIR) / input_value
+    )
+
+    # Read GUI image (PNG)
+    gui_image_png_base64: str = aim_utils.read_image(gui_image_filepath)
+
+    # Execute metric
+    result: Optional[List[Any]] = Metric5.execute_metric(gui_image_png_base64)
+
+    # Test result
+    if result is not None:
+        assert result[0] == expected_result[0]
