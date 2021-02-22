becd1059a3529b409adc401c040df838df1bfe90
mplaine <markku.laine@aalto.fi>
2021-02-22 19:15:00 +0200

    Added message models

--- backend/aim/handlers/handlers.py
+++ backend/aim/handlers/handlers.py
@@ -14,26 +14,26 @@ Handlers.
 import json
 import logging
 import pathlib
-import uuid
-from typing import Any, Awaitable, Dict, List, Optional, Union
+from typing import Any, Dict, Optional, Union
 from urllib.parse import urlparse
 
 # Third-party modules
 import tornado.ioloop
 import tornado.websocket
+from pydantic.error_wrappers import ValidationError
 from tornado.options import options
 
 # First-party modules
 from aim.core import image_utils
 from aim.core.constants import ALLOWED_HOSTS
-from aim.exceptions.exceptions import ValidationError
+from aim.models.models import MessageBase, MessageImage, MessageURL
 
 # ----------------------------------------------------------------------------
 # Metadata
 # ----------------------------------------------------------------------------
 
 __author__ = "Markku Laine"
-__date__ = "2021-02-17"
+__date__ = "2021-02-22"
 __email__ = "markku.laine@aalto.fi"
 __version__ = "1.0"
 
@@ -61,60 +61,27 @@ class AIMWebSocketHandler(tornado.websocket.WebSocketHandler):
 
         try:
             # Load message
-            msg: Dict[str, Any] = json.loads(message)
+            message_data: Dict[str, Any] = json.loads(message)
 
-            # Get message data
-            msg_type: Optional[str] = msg.get("type", None)
-            # msg_url: str = msg.get("url", "")
-            msg_filename: str = msg.get("filename", "").strip().lower()
-            # msg_metrics: Dict[str, bool] = {
-            #     k: v for k, v in msg.get("metrics", {}).items() if v is True
-            # }  # filter
-            msg_data: Optional[str] = msg.get("data", None)
+            # Create message base model
+            message_base: MessageBase = MessageBase(**message_data)
+
+            # Create variables
             # server_name: str = options.name
             # session_id: str = uuid.uuid4().hex
 
-            # Validate message data
-            if msg_type is None or msg_type != "execute":
-                raise ValidationError(
-                    "Unsupported message type: '{}'".format(msg_type)
-                )
-
             # Input: URL
-            if msg_data is None:
+            if message_base.data is None:
                 pass
             # Input: image
             else:
-                # Validate message filename
-                if msg_filename == "":
-                    raise ValidationError("Message filename is required")
-                # Validate message filename extension
-                msg_filename_extension: str = pathlib.Path(msg_filename).suffix
-                if msg_filename_extension != ".png":
-                    raise ValidationError(
-                        "Invalid message filename extension: '{}'".format(
-                            msg_filename_extension
-                        )
-                    )
-                # Validate message data
-                if msg_data is None or msg_data == "":
-                    raise ValidationError("Message data is required")
-                # Validate message data format
-                msg_data_format: str = msg_data.split(",")[0][
-                    5:
-                ]  # remove scheme 'data:'
-                if msg_data_format != "image/png;base64":
-                    raise ValidationError(
-                        "Invalid message data format: '{}'".format(
-                            msg_data_format
-                        )
-                    )
-
-                # Get message data raw
-                msg_data_raw: str = msg_data.split(",")[1]
+                # Create message image model
+                message_image = MessageImage(**message_base.dict())
 
                 # Crop image
-                png_image_base64: str = image_utils.crop_image(msg_data_raw)
+                png_image_base64: str = image_utils.crop_image(
+                    message_image.raw_data
+                )
                 image_utils.write_image(
                     png_image_base64,
                     pathlib.Path(options.runtime_data_dir)
@@ -125,7 +92,7 @@ class AIMWebSocketHandler(tornado.websocket.WebSocketHandler):
                 {
                     "type": "error",
                     "action": "pushValidationError",
-                    "message": str(e),
+                    "message": e.errors(),
                 }
             )
 
---
+++ backend/aim/models/models.py
@@ -0,0 +1,163 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Models.
+"""
+
+
+# ----------------------------------------------------------------------------
+# Imports
+# ----------------------------------------------------------------------------
+
+# Standard library modules
+import pathlib
+from enum import Enum
+from typing import Dict, Optional, Union
+
+# Third-party modules
+from pydantic import AnyHttpUrl, BaseModel, Extra, Field, validator
+
+# ----------------------------------------------------------------------------
+# Metadata
+# ----------------------------------------------------------------------------
+
+__author__ = "Markku Laine"
+__date__ = "2021-02-22"
+__email__ = "markku.laine@aalto.fi"
+__version__ = "1.0"
+
+# ----------------------------------------------------------------------------
+# Enumerations
+# ----------------------------------------------------------------------------
+
+
+class MessageType(str, Enum):
+    execute = "execute"
+
+
+# ----------------------------------------------------------------------------
+# Models
+# ----------------------------------------------------------------------------
+
+# Shared configurations
+class MessageConfigBase(BaseModel):
+    """
+    The MessageConfigBase model.
+    """
+
+    class Config:  # see https://pydantic-docs.helpmanual.io/usage/model_config/
+        anystr_strip_whitespace = (
+            True  # strip leading and trailing whitespace for str
+        )
+        extra = (
+            Extra.forbid
+        )  # forbid extra attributes during model initialization
+        validate_all = True  # validate model field defaults
+        validate_assignment = (
+            True  # perform validation on assignment to attributes
+        )
+
+
+class MessageBase(MessageConfigBase):
+    """
+    The MessageBase model.
+    """
+
+    type: MessageType = Field(
+        ...,
+        description="The type of this message.",
+        example=MessageType.execute,
+    )
+    url: Union[str, AnyHttpUrl] = Field(
+        ..., description="The URL of this message.", example="https://aalto.fi"
+    )
+    data: Optional[str] = Field(
+        ...,
+        description="The data (PNG image encoded in Base64) of this message.",
+    )
+    filename: Optional[Union[pathlib.Path, str]] = Field(
+        ...,
+        description="The filename (PNG image) of this message.",
+        example="aalto.fi.png",
+    )
+    metrics: Dict[str, bool] = Field(
+        ...,
+        description="The metrics of this message.",
+        example={"m1": True, "m5": True},
+    )
+
+    @validator("metrics")
+    def process_metrics(cls, v: Dict[str, bool]):
+        metrics_filtered: Dict[str, bool] = {
+            k: v for k, v in v.items() if v is True
+        }  # filter
+        return metrics_filtered
+
+
+class MessageURL(MessageBase):
+    """
+    The MessageURL model.
+    """
+
+    url: AnyHttpUrl = Field(
+        ..., description="The URL of this message.", example="https://aalto.fi"
+    )
+    # data: Optional[str] = None  # Support for the None type will be in the next release https://github.com/samuelcolvin/pydantic/pull/2149
+    # filename: Optional[str] = None  # Support for the None type will be in the next release https://github.com/samuelcolvin/pydantic/pull/2149
+
+    @validator("data")
+    def data_must_be_none(cls, v: Optional[str]):
+        if v is not None:
+            raise ValueError("Data must be None")
+        return v
+
+    @validator("filename")
+    def filename_must_be_none(cls, v: Optional[str]):
+        if v is not None:
+            raise ValueError("Filename must be None")
+        return v
+
+
+class MessageImage(MessageBase):
+    """
+    The MessageImage model.
+    """
+
+    url: str = Field(
+        ...,
+        description="The URL of this message.",
+        example="https://aalto.fi",
+        min_length=0,
+        max_length=0,
+    )
+    data: str = Field(
+        ...,
+        description="The data (PNG image encoded in Base64) of this message.",
+    )
+    filename: pathlib.Path = Field(
+        ...,
+        description="The filename (PNG image) of this message.",
+        example="aalto.fi.png",
+    )
+    raw_data: str = Field(
+        "",
+        description="The raw data (PNG image encoded in Base64) of this message.",
+    )
+
+    def __init__(self, **data):
+        super().__init__(**data)
+        self.raw_data = self.data.split(",")[1]
+
+    @validator("data")
+    def data_must_be_image_png_base64(cls, v: str):
+        data_format: str = v.split(",")[0][5:]  # remove scheme 'data:'
+        if data_format != "image/png;base64":
+            raise ValueError("Data format must be 'image/png;base64'")
+        return v
+
+    @validator("filename")
+    def file_extension_must_be_png(cls, v: pathlib.Path):
+        if v.suffix != ".png":
+            raise ValueError("File extension must be '.png'")
+        return v
