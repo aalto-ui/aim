68c841ed6e7228ce6cb7d239a5d534d06aa5976a
Markku Laine <markku.laine@aalto.fi>
2021-10-14 10:59:26 +0300

    docs: 📝 update the list of contributors in web app GUI

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -76,7 +76,7 @@
                   </p>
                   <h6>Contributors</h6>
                   <p>
-                    Antti Oulasvirta, Samuli De Pascale, Janin Koch, Thomas Langerak, Jussi Jokinen, Kashyap Todi, Markku Laine, Manoj Kristhombuge, Yuxi Zhu, Aliaksei Miniukovich, Gregorio Palmas, and Tino Weinkauf, Ai Nakajima, and Valentin Ionita.
+                    Antti Oulasvirta, Samuli De Pascale, Janin Koch, Thomas Langerak, Jussi Jokinen, Kashyap Todi, Markku Laine, Manoj Kristhombuge, Yuxi Zhu, Aliaksei Miniukovich, Gregorio Palmas, Tino Weinkauf, Ai Nakajima, Valentin Ionita, Morteza Shiripour, and Amir Hossein Kargaran.
                   </p>
                 </b-col>
               </b-row>
